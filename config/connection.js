e7fa164e44fb80562d752b59dfbd236764ad0005
olliedirker <olliedirker@gmail.com>
2022-08-25 19:14:44 -0500

    figured out why it wouldnt host

--- config/connection.js
+++ config/connection.js
@@ -3,11 +3,14 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-//create connection to our database, pass in your MySQL information for username and password
+// create connection to our db
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
+  host: 'localhost',
+  user: 'dirker',
+  password:'Lorelei09072017!',
+  dialect: 'mysql',
+  port: 3306
 });
 
 module.exports = sequelize;
+
